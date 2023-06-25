465c0161bebdc3b90df43a3f02208a3d7011cb70
Shiroth Shiyamalan <128440990+Shiroth07@users.noreply.github.com>
2023-06-25 16:49:47 -0400

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+var typed= new Typed(".text",{
+    strings:["Student @FMMCA", "Front-end Developer", "Web Developer", "Tutor", "Entrepreneur", "Volunteer", "Athlete"],
+    typeSpeed: 100,
+    backSpeed: 75,
+    backDelay: 1000,
+    loop: true, 
+
+
+});
+
